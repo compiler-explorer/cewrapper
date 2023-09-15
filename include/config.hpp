42930dd61e48dd464fdc761cdda468db01ff678f
Patrick Quist <partouf@gmail.com>
2023-09-15 21:40:44 +0200

    Update config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -10,8 +10,8 @@ namespace cewrapper
 
 struct Config
 {
-    bool debugging{true};
-    bool extra_debugging{true};
+    bool debugging{false};
+    bool extra_debugging{false};
     bool suspend_after_start{};
     int time_limit_ms{};
     int loopwait_ms{ 500 };
