ef90cd0fd2b2074c832e8c51d757c18ca2b21421
Partouf <partouf@gmail.com>
2025-10-08 23:36:14 +0200

    .

--- include/access.hpp
+++ include/access.hpp
@@ -6,6 +6,8 @@
 namespace cewrapper
 {
 
+void allow_access_to_nul(wchar_t *container_sid);
+
 void grant_access_to_path(wchar_t *container_sid, wchar_t *dir, uint32_t permissions);
 void grant_access_to_registry(wchar_t *container_sid, wchar_t *key, uint32_t permissions, registry_type_t regtype);
 
